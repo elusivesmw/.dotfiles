bb806e17520d4cb3ef09460585365e5aff8187b5
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-08-22 04:06:27 -0700

    add tmux and install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+# create symlinks
+ln -s ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/.config/nvim/lua/elusive.lua ~/.config/nvim/lua/elusive.lua
+ln -s ~/.dotfiles/.tmux.conf ~.tmux.conf
+ln -s ~/.dotfiles/.zshrc ~.zshrc
+# do this better
