6101745eb3d44c8b5fbc6a9941ce3b9f8aafc93d
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-11-26 20:35:57 -0800

    couple remaps

--- .config/nvim/lua/elusive/remap.lua
+++ .config/nvim/lua/elusive/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 
+-- in and out of insert
+vim.keymap.set("i", "<C-c>", "<esc>")
+vim.keymap.set("n", "<C-c>", "a")
+
 -- prev/next buffers
 vim.keymap.set("n", "<C-j>", "<cmd>bn<cr>")
 vim.keymap.set("n", "<C-k>", "<cmd>bp<cr>")
@@ -8,3 +12,12 @@ vim.keymap.set("n", "<C-k>", "<cmd>bp<cr>")
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
 
+-- formatting
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+-- close buffer
+vim.keymap.set("n", "<leader>x", "<cmd>bd<cr>")
+-- easier write
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
