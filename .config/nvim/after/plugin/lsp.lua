80f11fdd3966b4534e337436e1dbb58a909983bc
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2023-12-09 10:54:26 -0800

    update keybindings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,18 +1,25 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local tele = require('telescope.builtin')
 
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+  vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set('n', 'gr', function() tele.lsp_references() end, opts)
+  vim.keymap.set({'i', 'n'}, '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = true}) end, opts)
+  vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, opts)
+
+  vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
+
 end)
 
 require('mason').setup({})
@@ -27,9 +34,6 @@ require('mason-lspconfig').setup({
   }
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
 cmp.setup({
   sources = {
     {name = 'path'},
@@ -38,7 +42,8 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<Space>'] = cmp.mapping.confirm({ select = false }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
 })
