606742f28d84a23f6a4887a2ef147281a251bfab
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-02 12:44:10 -0800

    link whole .config dir

--- symlink.sh
+++ symlink.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 
-# create missing dirs
-ds=(
-    ~/.config/nvim
-    ~/.config/nvim/lua
-    ~/.config/nvim/after
-    ~/.config/nvim/after/plugin
-)
-for d in ${ds[@]}; do
-    mkdir -p $d
-done
-
 # create symlinks
 links=(
-    .config/nvim/init.lua
-    .config/nvim/lua/elusive/init.lua
-    .config/nvim/lua/elusive/global.lua
-    .config/nvim/lua/elusive/packer.lua
-    .config/nvim/lua/elusive/parens.lua
-    .config/nvim/lua/elusive/remap.lua
-    .config/nvim/lua/elusive/set.lua
-    .config/nvim/after/plugin/fugitive.lua
-    .config/nvim/after/plugin/lsp.lua
-    .config/nvim/after/plugin/telescope.lua
-    .config/nvim/after/plugin/treesitter.lua
-    .config/nvim/after/plugin/undotree.lua
+    .config
     .gitconfig
     .tmux.conf
     .zshrc
