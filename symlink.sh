d8a9169a2a87002255974784b0526dc2989415bc
elusivesmw <elusivesmw@gmail.com>
2024-01-03 02:52:10 -0800

    add linkfile and linkdir functions

--- symlink.sh
+++ symlink.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
+function linkfile {
+    file="$1"
+    if [ ! -e ~/$file ] && [ ! -L ~/$file ] ; then
+        echo "$file not found, linking..."
+        ln -sf ~/.dotfiles/$file ~/$file
+    else
+        echo "$file found, skipping..."
+    fi
+}
+
+function linkdir {
+    dir="$1"
+    # handle existing directories 
+    if [ -d ~/$dir ]; then
+        if [ ! -L ~/$dir ]; then
+            echo "backing up ~/$dir"
+            mv ~/$dir ~/${dir}_old
+        else
+            echo "$dir link found, skipping"
+            return
+        fi
+    fi
+
+    # create dir symlink
+    echo "$dir, linking"
+    ln -sf ~/.dotfiles/$dir ~/$dir
+}
+
 # create .config dir
 mkdir -p ~/.config
 
-# backup existing non-linked nvim dir
-if [ -d ~/.config/nvim ] && [ ! -L ~/.config/nvim ]; then
-    echo "backup ~/.config/nvim"
-    mv ~/.config/nvim ~/.config/nvim_old
-fi
+# link nvim dir
+linkdir .config/nvim
 
-# create nvim dir symlink
-ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
-
-# create symlinks
-links=(
+# link files
+files=(
     .gitconfig
     .tmux.conf
     .zshrc
 )
-for link in ${links[@]}; do
-    ln -sf ~/.dotfiles/$link ~/$link
+for file in ${files[@]}; do
+    linkfile $file
 done
 
