af1eb964474e03487558294bc0803687a66048e7
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-03 22:06:55 -0800

    Linkdir (#1)
    * add linkfile and linkdir functions

--- symlink.sh
+++ symlink.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-# backup nvim dir if exists
-if [ -d "~/.config/nvim" ]; then
-    mv ~/.config/nvim ~/.config/nvim_old
-fi
+function linkfile {
+    file="$1"
+    if [ ! -e ~/$file ] && [ ! -L ~/$file ] ; then
+        echo "$file not found, linking..."
+        ln -sf ~/.dotfiles/$file ~/$file
+    else
+        echo "$file found, skipping..."
+    fi
+}
 
-# nvim dir symlink
-ln -sf ~/.dotfiles/.config/nvim ~/.config
+function linkdir {
+    dir="$1"
+    # handle existing directories 
+    if [ -d ~/$dir ]; then
+        if [ ! -L ~/$dir ]; then
+            echo "backing up ~/$dir"
+            mv_merge ~/$dir ~/${dir}_old
+        else
+            echo "$dir link found, skipping"
+            return
+        fi
+    fi
 
-# create symlinks
-links=(
+    # create dir symlink
+    echo "$dir, linking"
+    ln -sf ~/.dotfiles/$dir ~/$dir
+}
+
+function mv_merge {
+    source="$1"
+    target="$2"
+    cp -rl $source/. $target
+    rm -r $source
+}
+
+# create .config dir
+mkdir -p ~/.config
+
+# link nvim dir
+linkdir .config/nvim
+
+# link files
+files=(
     .gitconfig
     .tmux.conf
     .zshrc
 )
-for link in ${links[@]}; do
-    ln -sf ~/.dotfiles/$link ~/$link
+for file in ${files[@]}; do
+    linkfile $file
 done
 
