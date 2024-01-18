5dca30e6b24e20fa4e282088ca919f80b2b7dfd0
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-01-17 20:42:49 -0800

    update themes

--- .config/nvim/lua/elusive/plugins/theme.lua
+++ .config/nvim/lua/elusive/plugins/theme.lua
@@ -1,5 +1,33 @@
 return {
-    { "catppuccin/nvim", as = "catppuccin" },
-    { "bluz71/vim-moonfly-colors", as = "moonfly" },
+    {
+        "folke/tokyonight.nvim",
+        as = "tokyonight",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+            })
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        as = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+            })
+        end,
+    },
+    {
+        "bluz71/vim-moonfly-colors",
+        as = "moonfly",
+        lazy = false,
+        priority = 1000,
+        config = function() end,
+    },
     "nvim-tree/nvim-web-devicons",
 }
