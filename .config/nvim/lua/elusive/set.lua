3e874850961bfbf975cd7da819205a8af3c53b52
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-02-11 17:40:45 -0800

    set noshowmode

--- .config/nvim/lua/elusive/set.lua
+++ .config/nvim/lua/elusive/set.lua
@@ -1,12 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
 vim.opt.scrolloff = 10
 
 vim.opt.swapfile = false
@@ -22,6 +20,8 @@ vim.opt.listchars:append({
     tab = ">·",
     trail = "·",
 })
+
+vim.opt.showmode = false -- not needed with lualine
 
 vim.opt.termguicolors = true
 vim.cmd.colorscheme("moonfly")
