677e8fe201b0759050b1ac352e90a9db07da207d
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-03-10 16:49:31 -0700

    open a newline in between {}

--- .config/nvim/lua/elusive/parens.lua
+++ .config/nvim/lua/elusive/parens.lua
@@ -13,7 +13,7 @@ local curr_char = function(offset)
 end
 
 -- close parens
-local close = function(char)
+local close_parens = function(char)
     if curr_char() == char then
         Feedkeys("<esc>la")
     else
@@ -21,18 +21,35 @@ local close = function(char)
     end
 end
 
+-- if enter is pressed between {}, open a newline in between
+local cr_parens = function()
+    if
+        (curr_char(-1) == "(" and curr_char() == ")")
+        or (curr_char(-1) == "{" and curr_char() == "}")
+        or (curr_char(-1) == "[" and curr_char() == "]")
+    then
+        Feedkeys("<cr><esc>O")
+    else
+        Feedkeys("<cr>")
+    end
+end
+
 -- keymaps
 vim.keymap.set("i", "(", "()<esc>ha", {})
 vim.keymap.set("i", ")", function()
-    close(")")
+    close_parens(")")
 end, {})
 
 vim.keymap.set("i", "{", "{}<esc>ha", {})
 vim.keymap.set("i", "}", function()
-    close("}")
+    close_parens("}")
 end, {})
 
 vim.keymap.set("i", "[", "[]<esc>ha", {})
 vim.keymap.set("i", "]", function()
-    close("]")
+    close_parens("]")
+end, {})
+
+vim.keymap.set("i", "<cr>", function()
+    cr_parens()
 end, {})
