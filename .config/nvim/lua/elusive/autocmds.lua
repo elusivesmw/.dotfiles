b044505d7ef36662fcf027faad8fdf7d04684059
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-04-12 14:30:26 -0700

    feat: highlight on yank

--- .config/nvim/lua/elusive/autocmds.lua
+++ .config/nvim/lua/elusive/autocmds.lua
@@ -1,6 +1,15 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+augroup("highlightYank", { clear = true })
+autocmd("TextYankPost", {
+    desc = "Highlight when yanking text",
+    group = "highlightYank",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 -- 2 space tabs
 augroup("setIndent", { clear = true })
 autocmd("Filetype", {
