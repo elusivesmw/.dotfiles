9e7e4690888c09049d899122afe41d50b16b17e5
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2024-04-13 18:16:24 -0700

    add more telescope keymaps and descs

--- .config/nvim/lua/elusive/plugins/telescope.lua
+++ .config/nvim/lua/elusive/plugins/telescope.lua
@@ -25,17 +25,30 @@ local function config()
             },
         },
     })
-    vim.keymap.set("n", "<C-_>", builtin.current_buffer_fuzzy_find, {})
-    vim.keymap.set("n", "<C-g>", builtin.live_grep, {})
 
-    vim.keymap.set("n", "<C-f>", function()
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files" })
+
+    vim.keymap.set("n", "<leader><leader>", function()
+        builtin.buffers({
+            initial_mode = "normal",
+        })
+    end, { desc = "[ ] Search Buffers" })
+
+    vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Search current buffer" })
+
+    vim.keymap.set("n", "<leader>sf", function()
         builtin.find_files({
             find_command = find_command,
             cwd = utils.buffer_dir(),
             initial_mode = "normal",
-            --prompt_title = "Find Files in Current Dir",
         })
-    end, {})
+    end, { desc = "[S]earch [F]iles in current directory" })
 
     vim.keymap.set("n", "<leader>c", function()
         builtin.find_files({
@@ -43,7 +56,7 @@ local function config()
             cwd = vim.fn.stdpath("config"),
             initial_mode = "normal",
         })
-    end, {})
+    end, { desc = "Search [C]onfig" })
 
     vim.keymap.set("n", "<leader>fb", function()
         require("telescope").extensions.file_browser.file_browser({
@@ -51,11 +64,11 @@ local function config()
             select_buffer = true,
             initial_mode = "normal",
         })
-    end, {})
+    end, { desc = "[F]ile [B]rowser" })
 
     vim.keymap.set("n", "<leader>gf", function()
         builtin.git_files({ initial_mode = "normal" })
-    end, {})
+    end, { desc = "Search [G]it [F]iles" })
 end
 
 return {
