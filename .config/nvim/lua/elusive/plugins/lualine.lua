5ddf33bf7ee58b29735fa1c1af9549de17b3ffb9
elusivesmw <78516603+elusivesmw@users.noreply.github.com>
2025-07-04 18:13:02 -0700

    make lualine play nice with noice (show macros)

--- .config/nvim/lua/elusive/plugins/lualine.lua
+++ .config/nvim/lua/elusive/plugins/lualine.lua
@@ -1,5 +1,16 @@
 local function config()
-    require("lualine").setup()
+    require("lualine").setup({
+      sections = {
+        lualine_c = {
+          'filename',
+          {
+              require('noice').api.statusline.mode.get,
+              cond = require('noice').api.statusline.mode.has,
+              color = { ff = "ff9e64" }
+          }
+        }
+      }
+    })
 end
 
 return {
